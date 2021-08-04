a7feac650881bf16a92d66333e834e8c5fadb74d
BLCheung <blcheung@qq.com>
2021-08-04 23:18:32 +0800

    优化上传文件辅助类；

--- kit/upload-kit.js
+++ kit/upload-kit.js
@@ -1,18 +1,18 @@
-import URL                 from '@/consts/url/common'
-import { getPathFileName } from '@/utils/util'
-import ResultKit           from '@/kit/result-kit'
+import URL       from '@/consts/url/common';
+import util      from '@/utils/util';
+import ResultKit from '@/kit/result-kit';
 
 export default class UploadKit {
   constructor() {}
-
+  
   static uploadOneImage(src) {
     return new Promise((resolve) => {
       let fileName = 'default.jpg';
       uni.getImageInfo({
         src,
         success: (res) => {
-          console.log('res', res)
-          let name = getPathFileName(res.path);
+          console.log('res', res);
+          let name = util.getPathFileName(res.path);
           if (name && name != '') {
             fileName = name;
           }
@@ -38,16 +38,16 @@ export default class UploadKit {
         fail:    (res) => {
           resolve(ResultKit.Failed('网络开小差了~'));
         },
-      })
-    })
+      });
+    });
   }
-
+  
   static uploadMoreImage(files) {
     return new Promise((resolve, reject) => {
-      let i = 0;
-      let max = files.length;
+      let i      = 0;
+      let max    = files.length;
       let result = [];
-      console.log(files)
+      console.log(files);
       const _upload = () => {
         if (i < max) {
           uni.uploadFile({
@@ -75,18 +75,18 @@ export default class UploadKit {
         } else {
           resolve(ResultKit.OK(result));
         }
-      }
+      };
       _upload();
     });
   }
-
+  
   /**
    * 上传视频
    * @param {Object} src
    */
   static uploadOneVideo(src) {
     return new Promise((resolve) => {
-
+      
       // 上传图片
       uni.uploadFile({
         url:        URL.UPLOAD,
@@ -100,9 +100,9 @@ export default class UploadKit {
           if (res.statusCode !== 200) {
             return resolve(ResultKit.Failed('上传错误'));
           }
-          let result = res;
+          let result      = res;
           // zbl 11.20 修复返回的data是字符串问题
-          result.data = JSON.parse(result.data);
+          result.data     = JSON.parse(result.data);
           const resultKit = ResultKit.setResult(result);
           console.log('resultKit:', resultKit);
           if (!resultKit.isOK()) {
@@ -114,6 +114,6 @@ export default class UploadKit {
           return resolve(ResultKit.Failed('上传错误'));
         },
       });
-    })
+    });
   }
 }
