15c306a256d6c40dec99f96fd2c2074a0a4f2e1a
BLCheung <blcheung@qq.com>
2021-08-04 23:17:58 +0800

    优化任务基类发送网络请求方式；

--- models/base-model.js
+++ models/base-model.js
@@ -2,6 +2,7 @@ import ASY            from '@/base/asy';
 import HTTPKit        from '@/kit/http-kit';
 import PagingKit      from '@/kit/paging-kit';
 import HTTPMethodEnum from '@/enum/http-method-enum';
+import ResultKit      from '@/kit/result-kit';
 
 export default class BaseModel {
   /**
@@ -25,7 +26,7 @@ export default class BaseModel {
    * model.task [user.login]
    */
   static T(task) {
-    console.log(task)
+    console.log(task);
     return this.name() + '.' + task;
   }
   
@@ -56,7 +57,7 @@ export default class BaseModel {
       method,
       initData:  data,
       initQuery: query,
-    }
+    };
   }
   
   _taskList = {};
@@ -68,49 +69,44 @@ export default class BaseModel {
   /**
    * 发起一个网络请求任务
    * @param name 任务名称
-   * @param data 请求内容
-   * @param query
+   * @param data 任务数据
+   * @param query 任务url方式拼接参数
+   * @returns {ResultKit|*}
    */
   doTask(name, data = {}, query = {}) {
-    let taskItem = this._taskList[name]
-    ASY.assert(taskItem, '未定义网络任务:' + name)
+    let taskItem = this._taskList[name];
+    ASY.assert(taskItem, '未定义网络任务:' + name);
     let { url, method, initData, initQuery } = taskItem;
-    let newData                              = { ...initData, ...data }
-    let newQuery                             = { ...initQuery, ...query }
+    let newData                              = { ...initData, ...data };
+    let newQuery                             = { ...initQuery, ...query };
     
-    let newUrl = this._appendQueryParams(url, newQuery)
-    switch (method.toUpperCase()) {
-      case HTTPMethodEnum.GET:
-        return HTTPKit.GET(newUrl, newData);
-      case HTTPMethodEnum.POST:
-        return HTTPKit.POST(newUrl, newData);
-      case HTTPMethodEnum.DELETE:
-        return HTTPKit.DELETE(newUrl, newData);
-      case HTTPMethodEnum.PUT:
-        return HTTPKit.PUT(newUrl, newData);
-      default:
-        console.error(`未定义网络请求方式：${ method }`);
-        break;
+    let newUrl       = this._appendQueryParams(url, newQuery);
+    const methodType = method.toUpperCase();
+    if (!HTTPKit[methodType]) {
+      console.error(`未定义网络请求方式：${ method }`);
+      return ResultKit.Failed(`未定义网络请求方式：${ method }`, null);
     }
+    
+    return HTTPKit[methodType](newUrl, newData);
   }
   
   _appendQueryParams(url, query = {}) {
-    let and = '?'
+    let and = '?';
     if (url.indexOf('?') != -1) {
-      and = '&'
+      and = '&';
     }
-    return url + and + this._encodeQueryParams(query)
+    return url + and + this._encodeQueryParams(query);
   }
   
   _encodeQueryParams(obj) {
-    let params = []
+    let params = [];
     Object.keys(obj).forEach((key) => {
-      let value = obj[key]
+      let value = obj[key];
       if (typeof value === 'undefined') {
-        value = ''
+        value = '';
       }
-      params.push([ key, encodeURIComponent(value) ].join('='))
-    })
-    return params.join('&')
+      params.push([ key, encodeURIComponent(value) ].join('='));
+    });
+    return params.join('&');
   }
 }
